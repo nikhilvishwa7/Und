26075785ba90b9ac354d8cd9d05d906889a6badc
Aeditx03 <140535337+Aeditx03@users.noreply.github.com>
2023-08-12 21:14:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1967,8 +1967,13 @@ async def auto_filter(client, msg, spoll=False):
     #             await message.delete()
     # else:
     fuk = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
-    
-    await m.delete()
+    try : 
+        await m.delete()
+    except pyrogram.errors.exceptions.forbidden_403.MessageDeleteForbidden:
+    # Reply with the message indicating lack of delete permission
+        await message.reply_text("I don't have the rights to delete messages for everyone.please check my permissions")
+    # if spoll:
+    #     await msg.message.delete()        
     try:
         if settings['auto_delete']:
             await asyncio.sleep(300)
@@ -1979,6 +1984,9 @@ async def auto_filter(client, msg, spoll=False):
         await asyncio.sleep(300)
         await fuk.delete()
         await message.delete()
+    except pyrogram.errors.exceptions.forbidden_403.MessageDeleteForbidden:
+    # Reply with the message indicating lack of delete permission
+        await message.reply_text("I don't have the rights to delete messages for everyone.please check my permissions")
     # if spoll:
     #     await msg.message.delete()
 
