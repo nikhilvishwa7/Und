1a1ebedd106beedd9af858d637bbe7dd4f038c2a
Biisal <140535337+Aeditx03@users.noreply.github.com>
2023-09-14 18:31:01 +0530

    Update noverify.py

--- plugins/noverify.py
+++ plugins/noverify.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 import asyncio
 
-bot_uname = "Pm_link_yojana_bot"
+bot_uname = "Ai_Auto_Filter_Bot"
 
 # Define a dictionary to store user states
 user_states = {}
