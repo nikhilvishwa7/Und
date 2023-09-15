466fb608b3e83b4343e92dbb545e8ad2c2038865
Biisal <140535337+Aeditx03@users.noreply.github.com>
2023-09-15 21:19:45 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -63,8 +63,6 @@ async def save_group(bot, message):
                                                                          [[
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                                                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                                                                        ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Biisal")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
