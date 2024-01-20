097eb10db4052495067ab64826103a426017eabf
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-01-20 16:51:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,6 +23,7 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message[.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+    if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
                     InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url=CHNL_LNK),
                     InlineKeyboardButton('Gʀᴏᴜᴘ', url='https://t.me/+8mOqChNoVPA3MDE1'),
