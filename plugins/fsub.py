3d4e94b68f8b0cb5fb79d67c8e723130b3dbc54c
Jack <125857863+Singhsawan@users.noreply.github.com>
2024-01-24 12:12:18 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -119,21 +119,7 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
             )
         return False
 
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, update, file_id)
-        return fix_
-
-    except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
-        await update.reply(
-            text="Something went Wrong.",
-            parse_mode=enums.ParseMode.MARKDOWN,
-            disable_web_page_preview=True
-        )
-        return False
-
-
+    
 def set_global_invite(url: str):
     global INVITE_LINK
     INVITE_LINK = url
